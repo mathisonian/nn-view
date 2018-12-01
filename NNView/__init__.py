1e60df04285d1682794da7205ad9882f25461238
Matthew Conlen <mc@mathisonian.com>
2018-12-01 13:27:07 -0800

    get JS working somewhat

--- NNView/__init__.py
+++ NNView/__init__.py
@@ -1,6 +1,19 @@
 from os.path import join, dirname
+from IPython.display import display, HTML
 
 with open(join(dirname(__file__), 'VERSION')) as f:
     __version__ = f.read().strip()
 
 from . import models
+
+
+import codecs
+
+
+location = join(dirname(__file__), 'lib/nnview.js')
+js = codecs.open(location, "r", "utf-8").read()
+display(HTML('<script>' + js + '</script>'))
+
+
+
+# display(HTML('<script>window.a = 1234; console.log("b")</script>'))
--- NNView/models/pytorch.py
+++ NNView/models/pytorch.py
@@ -2,6 +2,8 @@
 
 from IPython.core.getipython import get_ipython
 from IPython.display import display, Javascript, HTML
+from vdom.helpers import h1, p, img, div, b
+from vdom.svg import svg, circle, rect
 
 
 class PyTorchModel(object):
@@ -9,11 +11,39 @@ class PyTorchModel(object):
     pass
 
   def __repr__(self):
-    return 'I am the representation of a pytorch model'
+
+    return svg(
+      circle(r=10, cx=20, cy=30, fill='#ff0000'),
+      width=500,
+      height=300
+    )
+    # display(
+    #   div(
+    #     svg(
+    #         circle(r=10, cx=20, cy=30, fill='#ff0000'),
+    #         width=500,
+    #         height=300
+    #     )
+    #   )
+    # )
+    # return ''
+    # return display(
+    #   div(
+    #     svg(
+    #       circle(r=10, cx=20, cy=30, fill='#ff0000'),
+    #       rect(width=30, height=30, x=200, y=300, fill='#0000ff'),
+    #       width=500,
+    #       height=300
+    #     )
+    #   )
+    # )
 
   def __str__(self):
     return 'I am a pytorch model'
 
   def display_diagram(self):
-    display(HTML("<b>:) okay?</b>"))
 
+    _id = 'graph-' + str(id(self))
+    js = 'lenet = LeNet("#' + _id + '"); console.log(lenet); lenet.redraw({ architecture: [{ numberOfSquares: 8, squareWidth: 128, stride: 8 }, { numberOfSquares: 8, squareWidth: 64, stride: 16 }, { numberOfSquares: 24, squareWidth: 48, stride: 8 }, { numberOfSquares: 24, squareWidth: 16, stride: 8 }], architecture2: [256, 128] }); lenet.redistribute({betweenLayers_: [40, 10, -20, -20] })'
+
+    display(HTML('<div><div id="' + _id + '"></div><script>' + js + '</script></div>'))
