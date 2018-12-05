89990cdac2edb7b43cbf600fabd285a1681868bb
Eunice Jun <eunice.m.jun@gmail.com>
2018-12-05 11:55:40 -0800

    Add formatted list of Layer objects for each layer in original trace

--- NNView/models/pytorch.py
+++ NNView/models/pytorch.py
@@ -7,15 +7,23 @@ from IPython.core.getipython import get_ipython
 from IPython.display import display, Javascript, HTML
 from vdom.helpers import h1, p, img, div, b
 from vdom.svg import svg, circle, rect
+from NNView.layers.layer import Layer
+
+
+LAYER_TYPES = ['Linear', 'Conv']
+LAYER_TYPES_FEATURES = [('in_features', 'out_features'), ('in_channels', '')]
 
 class PyTorchModel(object):
   def __init__(self, net, net_input):
     self.network = net
     self.input = net_input
-
+    
     # Override forward pass and get layer order and info
-    self.trace = self.trace_net(self.network)
-    self.network(self.input)
+    # self.trace = self.trace_net(self.network)
+    self.trace = self.trace_net()
+
+    self.network(self.input) # run forward pass of networks
+    self.trace = self.format_trace(self.trace)
 
   def __repr__(self):
 
@@ -52,13 +60,14 @@ class PyTorchModel(object):
   def _mk_wrapper(self, trace, super_method):
     def _wrapper(self, *args, **kwargs):
         output = super_method(*args, **kwargs)
-        trace.append((self, args, kwargs, output))
+        trace.append(self)
+        # trace.append((self, args, kwargs, output))
         return output
     return _wrapper
+
   # Binds newly defined wrapper to network layers so that we can re-create  layer order and input/output
   # @Returns a list where each element is a tuple:
-
-  def trace_net(self, net):
+  def trace_net(self):
     trace = []
     for child in net.children():
         old_call = child.forward
@@ -66,8 +75,44 @@ class PyTorchModel(object):
         child.forward = types.MethodType(new_method, child)
     return trace 
 
-  def format_trace(self):
-    pass
+  # @param trace is list of raw layer output
+  def format_trace(self, trace):
+      formatted = []
+
+      # for i,l in enumerate(trace):
+      i = 0
+      while i < len(trace):
+        l = trace[i]
+        layer_type = l.__class__.__name__
+        input_shape = None
+        output_shape = None
+        activation = None
+        kernel = None
+        stride = None
+
+        # check if next layer is an activation function 
+        if (i+1 < len(trace) and isinstance(trace[i+1], torch.nn.modules.activation.Threshold)):
+            activation = trace[i+1].__class__.__name__
+            i += 1 # skip the activation function
+        
+        if ('Linear' in layer_type): 
+            input_shape = l.__dict__['in_features']
+            output_shape = l.__dict__['out_features']
+        elif ('Conv' in layer_type): # Allows for Conv1d, Conv2d, Conv3d
+            input_shape = l.__dict__['in_channels']
+            output_shape = l.__dict__['out_channels']
+            kernel = l.__dict__['kernel_size'][0] # without [0], returns tuple - e.g., (3,)
+            stride = l.__dict__['stride'][0] # without [0], returns tuple - e.g., (3,)
+            
+        else: 
+          raise Exception(f"Layer type not supported: {layer_type}")
+
+        layer = Layer(layer_type, input_shape, output_shape, activation, kernel, stride)
+        formatted.append(layer)
+        # import pdb; pdb.set_trace()
+        i+=1
+      return formatted
+    
 
   def display_diagram(self):
     self.trace
@@ -87,3 +132,30 @@ class PyTorchModel(object):
         print("Arg Dimesions: ", [x.shape for x in args])
         print("Output Dimesions: ", output.shape)
         print("-" * 10)
+
+
+class Net(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+        self.c1 = nn.Conv1d(16, 33, 3, stride=2)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.c1(torch.randn(20, 16, 50))
+        return out
+
+input_size = 784
+hidden_size = 500
+num_classes = 10
+
+net = Net(input_size, hidden_size, num_classes)
+net_input = torch.randn(input_size)
+ptm = PyTorchModel(net, net_input)
+
+## TRY for nn.Sequential type
+import pdb; pdb.set_trace()
