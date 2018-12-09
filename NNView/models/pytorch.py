ef0385c38764720b697b9ddd78bbc94894f2c27b
Eunice Jun <eunice.m.jun@gmail.com>
2018-12-09 14:20:02 -0800

    Adjust pytorch input and output dimensions

--- NNView/models/pytorch.py
+++ NNView/models/pytorch.py
@@ -107,8 +107,8 @@ class PyTorchModel(object):
   def _mk_wrapper(self, trace, super_method):
     def _wrapper(self, *args, **kwargs):
         output = super_method(*args, **kwargs)
-        trace.append(self, args)
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
+        trace.append((self, args[0]))
         # trace.append((self, args, kwargs, output))
         return output
     return _wrapper
@@ -125,33 +125,33 @@ class PyTorchModel(object):
 
   # @param trace is list of raw layer output
   def format_trace(self, trace):
-      size_info = trace[1].shape[0]
-      layer_info = trace[0]
-
       formatted = []
       
       # for i,l in enumerate(trace):
       i = 0
-      while i < len(layer_info):
-        l = layer_info[i]
+      while i < len(trace):
+        l = trace[i][0]
+        size = trace[i][1].shape
+
         layer_type = l.__class__.__name__
         input_shape = None
         output_shape = None
+        channels = None
         activation = None
         kernel = None
         stride = None
 
         # check if next layer is an activation function
-        if (i+1 < len(layer_info) and isinstance(layer_info[i+1], torch.nn.modules.activation.Threshold)):
-            activation = layer_info[i+1].__class__.__name__
+        if (i+1 < len(trace) and isinstance(trace[i+1][0], torch.nn.modules.activation.Threshold)):
+            activation = trace[i+1][0].__class__.__name__
             i += 1 # skip the activation function
 
         if ('Linear' in layer_type):
-            input_shape = size_info[1]
+            input_shape = size[len(size) - 1]
             output_shape = l.__dict__['out_features']
         elif ('Conv' in layer_type): # Allows for Conv1d, Conv2d, Conv3d
-            input_shape = size_info[2]
-            output_shape = None
+            input_shape = size[len(size) - 1]
+            output_shape = None # size[len(size) - 1]
             channels = l.__dict__['out_channels']
             kernel = l.__dict__['kernel_size'][0] # without [0], returns tuple - e.g., (3,)
             stride = l.__dict__['stride'][0] # without [0], returns tuple - e.g., (3,)
@@ -187,29 +187,71 @@ class PyTorchModel(object):
 
 
 
-## TESTING
+## TESTING 1 
+# class Net(nn.Module):
+#     def __init__(self, input_size, hidden_size, num_classes):
+#         super(Net, self).__init__()
+#         self.fc1 = nn.Linear(input_size, hidden_size)
+#         self.relu = nn.ReLU()
+#         self.fc2 = nn.Linear(hidden_size, num_classes)
+#         self.c1 = nn.Conv1d(16, 33, 3, stride=2)
+
+#     def forward(self, x):
+#         out = self.fc1(x)
+#         out = self.relu(out)
+#         out = self.fc2(out)
+#         out = self.c1(torch.randn(20, 16, 50))
+#         return out
+
+# input_size = 784
+# hidden_size = 500
+# num_classes = 10
+
+# net = Net(input_size, hidden_size, num_classes)
+# net_input = torch.randn(input_size)
+# ptm = PyTorchModel(net, net_input)
+
+# ## TRY for nn.Sequential type
+# import pdb; pdb.set_trace()
+
+
+## TESTING 2
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 class Net(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
+
+    def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, num_classes)
-        self.c1 = nn.Conv1d(16, 33, 3, stride=2)
+        # 1 input image channel, 6 output channels, 5x5 square convolution
+        # kernel
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        out = self.fc1(x)
-        out = self.relu(out)
-        out = self.fc2(out)
-        out = self.c1(torch.randn(20, 16, 50))
-        return out
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # If the size is a square you can only specify a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
-input_size = 784
-hidden_size = 500
-num_classes = 10
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
 
-net = Net(input_size, hidden_size, num_classes)
-net_input = torch.randn(input_size)
+
+net = Net()
+net_input = torch.randn(1, 1, 32, 32)
 ptm = PyTorchModel(net, net_input)
-
-## TRY for nn.Sequential type
-import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace()
